9d391477265ba96ee2872c795ea53f58549ce39c
sam <sam@freighttrust.com>
2020-08-01 03:20:45 -0700

    feat(toolchain): scripts for generic usage

---
+++ scripts/git-patch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# use patch, not diff
+if [ -z "$1" ] 
+then
+  echo "You need to supply a URL to a patch file."
+  exit
+fi
+ 
+URL=$1;
+ 
+# Download a patch and apply it.
+curl $URL | git apply -v --index
+# Commit patch and add a log message.
+git commit -m "Applied ${URL##*/}."
